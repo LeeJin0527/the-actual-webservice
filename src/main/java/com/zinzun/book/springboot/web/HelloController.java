5569cfb7a3c0dd13c35e1fb4cb940549840b119b
LeeJin0527 <ijin17463@gmail.com>
2022-03-11 17:15:48 +0900

    HelloController 롬복으로 전환
    MockMvc는 실제 객체와 비슷하지만 테스트에 필요한 기능만 가지는 가짜 객체를 만들어서 애플리케이션 서버에 배포하지 않고도 스프링 MVC 동작을 재현할 수 있는 클래스
    즉 가짜 객체 만든 후 API테스트를 수행하는 클래스

--- src/main/java/com/zinzun/book/springboot/web/HelloController.java
+++ src/main/java/com/zinzun/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.zinzun.book.springboot.web;
 
+import com.zinzun.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // RestController: 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어준다. @ResponseBody를 각 메소드마다 선언했던 것을 한번에 사용할 수 있게 해준다.
@@ -12,5 +14,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloResponseDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
     }
 }
