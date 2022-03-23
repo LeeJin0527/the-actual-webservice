f290f5cb456e04a3348910aa3576db053ab253ea
LeeJin0527 <ijin17463@gmail.com>
2022-03-23 22:44:47 +0900

    구글 oauth 연동 로그인 기능 구현

---
+++ src/main/java/com/zinzun/book/springboot/service/config/auth/dto/OAuthAttributes.java
@@ -0,0 +1,53 @@
+package com.zinzun.book.springboot.service.config.auth.dto;
+
+import com.zinzun.book.springboot.web.domain.user.Role;
+import com.zinzun.book.springboot.web.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.jca.cci.object.MappingRecordOperation;
+import org.springframework.security.oauth2.client.web.server.UnAuthenticatedServerOAuth2AuthorizedClientRepository;
+
+import java.util.Map;
+
+@Getter
+public class OAuthAttributes {
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+    private String email;
+    private String picture;
+
+    @Builder
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture){
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+    }
+
+
+
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes){
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+    public User toEntity(){
+        return User.builder()
+                .name(name)
+                .email(email)
+                .picture(picture)
+                .role(Role.GUEST)
+                .build();
+    }
+}
+
