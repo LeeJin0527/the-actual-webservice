d69a3caf13cb5f66ce7fffe7b8da6cdf4ea81d0f
LeeJin0527 <ijin17463@gmail.com>
2022-03-24 00:47:32 +0900

    시큐리티 적용 테스트

--- src/main/java/com/zinzun/book/springboot/Application.java
+++ src/main/java/com/zinzun/book/springboot/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // 스프링 부트의 자동설정, 스프링 빈 읽기와 생성을 자동으로 해줌
-@EnableJpaAuditing
+//@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
