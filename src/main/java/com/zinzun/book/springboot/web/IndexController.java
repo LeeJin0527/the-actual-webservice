9f1859e4771957ed1ede7c40f9e333fbf6e88a47
LeeJin0527 <ijin17463@gmail.com>
2022-03-24 20:37:51 +0900

    aws error fix

--- src/main/java/com/zinzun/book/springboot/web/IndexController.java
+++ src/main/java/com/zinzun/book/springboot/web/IndexController.java
@@ -7,11 +7,11 @@ import com.zinzun.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.lang.model.element.ModuleElement;
+
 import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
